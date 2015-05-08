1ab1dd5ba519f3bf1c7d1a3b94fb08189bc1853f
Steven Kauyedauty <skauyedauty@gmail.com>
2015-05-08 11:01:51 -0600

    ui router

---
+++ app.js
@@ -0,0 +1,76 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+         // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: 'mainController'
+            // controller: function($scope) {
+                
+            // }
+        })
+
+    // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+        url: '/about',
+        views: {
+
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'partial-about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'Look I am a column!' },
+
+            // for column two, we'll define a separate controller 
+            'columnTwo@about': { 
+                templateUrl: 'table-data.html',
+                controller: 'scotchController'
+            }
+        }
+        
+    });
+        
+});
+
+routerApp.controller('mainController',function($scope,$state){
+    $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+                console.log('connected');
+                console.log($scope.dogs);
+})
+
+routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
